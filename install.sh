be1088131b05617663106907c782fc5054294d69
Sage Wall <sagewall@users.noreply.github.com>
2021-10-23 17:28:42 -0600

    🔧 Update dotfiles

--- install.sh
+++ install.sh
@@ -10,16 +10,6 @@ sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y
 
-# Installing google chrome
-echo "Installing google chrome"
-if [ ! -d "~/Downloads" ] 
-then
-    echo "Creating ~/Downloads directory"
-    mkdir ~/Downloads
-fi
-wget --quiet --output-document ~/Downloads/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install ~/Downloads/google-chrome-stable_current_amd64.deb -y
-
 # Configuring zsh with the powerlevel10k theme
 echo "Configuring zsh with the powerlevel10k theme"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -33,20 +23,5 @@ cp .gitconfig ~/.gitconfig
 # Configuring vim
 echo "Configuring vim"
 cp -r .vim ~/
-
-# Configuring nvm
-echo "Configuring nvm"
-rm ~/.npmrc
-nvm install --lts
-nvm use --lts
-
-# Running conda init for bash and zsh
-echo "Running conda init for bash and zsh"
-conda init bash
-conda init zsh
-
-# Update Anaconda
-echo "Updating Anaconda"
-sudo conda update conda -y
 
 echo "Done with dotfiles install script"
